dfbe5f93f9aa1c40e3b64ca29f70f5eceeabc10f
Ihor Holohots <igor.gologots@hyuna.bb>
2019-01-30 15:40:52 +0200

    Clean project

---
+++ app/js/common.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	/* Parallax 
+	$('.parallax-window').parallax({imageSrc: 'img/background.jpg'});
+	*/
+	/* Resize 
+	function heightDetect(){
+		$(".parallax-window").css("height", $(window).height());	
+	}
+	heightDetect();
+	$(window).resize(function(){
+		heightDetect();
+	});
+	*/
+});
