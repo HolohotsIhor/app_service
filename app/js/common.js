b8f504aedce4fd6dd82c862aa5277e29e2b33573
Ihor Holohots <igor.gologots@hyuna.bb>
2019-02-06 18:47:58 +0200

    Added product box

--- app/js/common.js
+++ app/js/common.js
@@ -1,43 +1,43 @@
 $(document).ready(function() {
-	/* Parallax 
-	$('.parallax-window').parallax({imageSrc: 'img/background.jpg'});
-	*/
-	/* Resize 
-	function heightDetect(){
-		$(".parallax-window").css("height", $(window).height());	
-	}
-	heightDetect();
-	$(window).resize(function(){
-		heightDetect();
-	});
-	*/
-	
-	/* Burger */
-   $(".toggle_mnu").click(function() {
-    $(".sandwich").toggleClass("active");
-});
-   /* Burger. Hover */
-   $(".top_mnu ul a").click(function() {
-    $(".top_mnu").fadeOut(600);
-    $(".sandwich").toggleClass("active");
-    $(".top_text").css("opacity", "1");
-}).append("<span>");
+    /* Parallax 
+    $('.parallax-window').parallax({imageSrc: 'img/background.jpg'});
+    */
+    /* Resize 
+    function heightDetect(){
+        $(".parallax-window").css("height", $(window).height());    
+    }
+    heightDetect();
+    $(window).resize(function(){
+        heightDetect();
+    });
+    */
 
-   /* Burger. Open and close */
-   $(".toggle_mnu").click(function() {
-    if ($(".top_mnu").is(":visible")) {
-        $(".top_text").css("opacity", "1");
+    /* Burger */
+    $(".toggle_mnu").click(function() {
+        $(".sandwich").toggleClass("active");
+    });
+    /* Burger. Hover */
+    $(".top_mnu ul a").click(function() {
         $(".top_mnu").fadeOut(600);
-        $(".top_mnu li a").removeClass("fadeInUp animated");
-    } else {
-        $(".top_text").css("opacity", ".1");
-        $(".top_mnu").fadeIn(600);
-        $(".top_mnu li a").addClass("fadeInUp animated");
-    };
-});
+        $(".sandwich").toggleClass("active");
+        $(".top_text").css("opacity", "1");
+    }).append("<span>");
 
-   /* Slider */
-   $('.carousel').carousel({
-      interval: 4000
-  })
+    /* Burger. Open and close */
+    $(".toggle_mnu").click(function() {
+        if ($(".top_mnu").is(":visible")) {
+            $(".top_text").css("opacity", "1");
+            $(".top_mnu").fadeOut(600);
+            $(".top_mnu li a").removeClass("fadeInUp animated");
+        } else {
+            $(".top_text").css("opacity", ".1");
+            $(".top_mnu").fadeIn(600);
+            $(".top_mnu li a").addClass("fadeInUp animated");
+        };
+    });
+
+    /* Slider */
+    $('.carousel').carousel({
+        interval: 4000
+    })
 });
