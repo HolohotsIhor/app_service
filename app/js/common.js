18307be27cde90679cedb551af048d29876993ed
Ihor Holohots <igor.gologots@hyuna.bb>
2019-02-15 18:28:25 +0200

    Added calculator

--- app/js/common.js
+++ app/js/common.js
@@ -1,16 +1,12 @@
 $(document).ready(function() {
-    /* Parallax 
-    $('.parallax-window').parallax({imageSrc: 'img/background.jpg'});
-    */
-    /* Resize 
-    function heightDetect(){
-        $(".parallax-window").css("height", $(window).height());    
-    }
-    heightDetect();
-    $(window).resize(function(){
-        heightDetect();
-    });
-    */
+    "use strict";
+
+    /* Button call */
+    $(".log-in").click(
+        function() {
+            $(".panel_call").animate({ width: 'toggle' }, 350)
+            return false;
+        })
 
     /* Burger */
     $(".toggle_mnu").click(function() {
@@ -38,11 +34,11 @@ $(document).ready(function() {
 
     /* Pop up */
     $(".pop-up-show").click(function() {
-        $(".pop-up-box").css('display','block');
+        $(".pop-up-box").css('display', 'block');
     })
 
     $(".close-box").click(function() {
-        $(".pop-up-box").css('display','none');
+        $(".pop-up-box").css('display', 'none');
     })
 
     /* Slider */
@@ -52,26 +48,46 @@ $(document).ready(function() {
 
     /* Scrool animation */
     $(window).scroll(function() {
-        $('.mov').each(function(){
-          var imagePos = $(this).offset().top;
-          var topOfWindow = $(window).scrollTop();
-          if (imagePos < topOfWindow+600) {
-            $(this).addClass('fadeIn');
-            $(this).css('opacity','1');
-        }
+        $('.mov').each(function() {
+            var imagePos = $(this).offset().top;
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow + 600) {
+                $(this).addClass('fadeIn');
+                $(this).css('opacity', '1');
+            }
+        });
     });
-    });
-
-    /* Button call */
-    $(document).ready(
-        function () {
-            $(".log-in").click(
-                function () {
-                    $(".panel_call").animate({width:'toggle'}, 350)
-                    return false;
-                }
-                )
-        }
-        );
 });
 
+
+$(document).ready(function() {
+
+    /* ***************Memory controlls*************** */
+    $('.memory-list li').click(function(){
+        $('.memory-list li').css('border-color', '#eee');
+        $(this).css('border-color', '#212121');
+        $('.memory-list li').removeClass();
+        $(this).addClass('active');
+        var memorySize = $(this).text(); /* Variables size */
+    });
+
+    /* ***************Status controlls*************** */
+    $('.status-list li').click(function(){
+        $('.status-list li').css('border-color', '#eee');
+        $(this).css('border-color', '#212121');
+        $('.status-list li').removeClass();
+        $(this).addClass('active');
+    });
+
+    /* ***************Model controlls*************** */
+        $("#pa_versija-iphone").change(function() {
+        var gadgetModel = $("#pa_versija-iphone option:selected").val(); /* Variables model */
+        var modelPrice = 0; /* Variables price*/
+
+        if (gadgetModel == 'iphone-5') {
+            modelPrice = 382; 
+        }
+
+        return modelPrice;
+    });   
+});
